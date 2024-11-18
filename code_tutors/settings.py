cd58a16e27211a2b04b1fc2407ee1fd3f8dcb926
DannyJT27 <daniel.tierney4@gmail.com>
2024-11-18 17:36:50 +0000

    Update settings.py
    Undo removal of localhost from allowed hosts

--- code_tutors/settings.py
+++ code_tutors/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&$dln5wpgorppuw&(gintxm573v2ks+zq4o$(4*lapguixf^+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['teamhippo1.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', 'teamhippo1.pythonanywhere.com']
 
 
 # Application definition
