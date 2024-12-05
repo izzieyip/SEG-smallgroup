6756f0fef20a89f5ca09f1ad9c35d72db0045788
Aamukta Thogata <aamukta.thogata@gmail.com>
2024-12-05 15:19:36 +0000

    Add Basic Tests for form

---
+++ tutorials/tests/forms/test_create_new_admin_form.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from tutorials.forms import CreateNewAdminForm
+
+## tests for the 'create a new admin form'
+## first we test that the form contains the correct fields
+
+class CreateNewAdminFormTestCase(TestCase):
+    def test_from_has_necessary_fields(self):
+        form = CreateNewAdminForm()
+        self.assertIn('username', form.fields)
+        self.assertIn('firstname', form.fields)
+        self.assertIn('lastname', form.fields)
+        self.assertIn('password', form.fields)
+        self.assertIn('email', form.fields)
+
+
+    def test_create_new_admin_form_valid(self):
+        form_input = {
+            'firstname' : 'Ammu',
+            'lastname' : 'Bean',
+            'username' : '@AmmuBean',
+            'email' : 'ammubean@gmail.com',
+            'password' : 'Password123'
+        }
+        form = CreateNewAdminForm(data = form_input)
+        self.assertTrue(form.is_valid())
+
+
+
