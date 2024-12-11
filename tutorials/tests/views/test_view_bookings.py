773442c88e21b888aaf1be3c16c3de866e9e0d73
Danny Tierney <daniel.tierney4@gmail.com>
2024-12-11 17:12:56 +0000

    Tests (WIP)

--- tutorials/tests/views/test_view_bookings.py
+++ tutorials/tests/views/test_view_bookings.py
@@ -11,19 +11,17 @@ class BookingViewTestCase(TestCase):
     fixtures = ['tutorials/tests/fixtures/default_user.json']
 
     def setUp(self):
-        #Page setup with superuser login
+        #Setup with superuser login
         self.url = reverse('view_bookings')
-        self.user = User.objects.get(username='@johndoe') #NEEDS TO BE A SUPERUSER ONCE THE SUPERUSER CHECK IS IN PLACE
+        self.user = User.objects.get(username='@johndoe')
 
-    ''' below is actually needed for the create booking form stuff but ill leave it for now just in case
+        '''Needs a sample booking to test deleting it'''
         #Generate sample data for Student and Tutor
         self.student = Student.objects.create(
-            username = "@jamesstudent",
+            username = "@Jamesstudent",
             first_name="James", 
             last_name="Student", 
             email="jamesstudent@example.com", 
-            skill_to_learn="DJ",
-            difficulty_level="1"
         )
         self.tutor = Tutor.objects.create(
             username = "@professordoe",
@@ -31,13 +29,14 @@ class BookingViewTestCase(TestCase):
             last_name="Doe",
             email="profdoe@example.com", 
             skills="DJ",
-            experience_level="5"
+            experience_level="3"
         )
 
         #Generate sample Booking Request
         self.booking_request = Booking_requests.objects.create(
             student=self.student,
             subject="DJ",
+            difficulty=3,
             isConfirmed=False
         )
 
@@ -47,13 +46,13 @@ class BookingViewTestCase(TestCase):
             tutor=self.tutor,
             booking_date=date(2024, 12, 25),
             booking_time=time(12, 30)
-        )'''
+        )
 
     def test_view_bookings_url(self):
         self.assertEqual(self.url,'/view_bookings/')
 
     def test_get_view_bookings(self):
-        self.client.login(username=self.user.username, password='Password123') #NEEDS TO BE A SUPERUSER ^
+        self.client.login(username=self.user.username, password='Password123')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'view_bookings.html')
@@ -62,3 +61,13 @@ class BookingViewTestCase(TestCase):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_valid_filtering_with_result(self):
+        #makes confirmed_booking not appear
+        response = self.client.get(reverse('view_bookings'), {'sortby':'date', 'filterby':'difficulty', 'search':'5'})
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'James Student')
+
+    #FIXTURES NOT WORKING ??
+
+       
